f91c31efdb6682314f6127ff8213e7d4800c34ad
Beffen94 <121238957+Beffen94@users.noreply.github.com>
2023-07-26 19:19:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 2000; // Цена билета
+        int price = 2000; // Цена билета
         int y = 20; // Стоимость 1 бонуса
         int bonus = x / y;
         System.out.println("Количество бонусов: " + bonus);
